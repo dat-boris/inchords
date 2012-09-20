3d34654077266ea6f2cc0d00519e58845b16c920
SketchyTechky <boris.w.lau@gmail.com>
2012-09-19 22:10:31 -0700

    added basic parser for ukutabs

---
+++ userscript/ukutabs.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name       incords.com/ukutabs/0.1
+// @description    In page chord replacement
+// @include       http://ukutabs.com/*
+// ==/UserScript==
+
+(function() {
+    "use strict";
+
+    injectCode(
+        "$('.qoate-code span').each(function(data) {$(this).html(\"<img src='http://cdn1.ukutabs.com/chords/\"+$(this).html()+\".png' alt='Am chord' class='chord' width='61' height='90'>\");});"
+    );
+    
+    function injectCode(code) {
+        var e = document.createElement("script");
+        e.textContent = code;
+        document.body.appendChild(e);
+    }
+
+}())
+
+
